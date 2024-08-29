f32dc5c02f81aebab602c09c1b2693bb3104a773
Vishesh Dadhich <visheshdadhich20@gmail.com>
2024-08-29 20:13:37 +0530

    Basic Website rendering

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,35 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-
+import mongoose from "mongoose";
+import "dotenv/config";
+import path from "path";
 const app = express();
-const router = express.Router();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
-router.get("/test", (req: Request, res: Response) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+const MONGO_URL = process.env.MONGO_URL || "";
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("MONGO CONNECTED");
+  })
+  .catch(() => {
+    console.log("MONGO CONNECTION ISSUE");
+  });
+
+app.get("/test", (req: Request, res: Response) => {
   res.send("Hiiiii");
 });
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 app.listen(2000, () => {
-  console.log("Port Activated");
+  console.log("Port 2000 Activated");
 });
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
